6a56d4a850c6542e420063bf6a7d81458c01f9de
Chitiz Awasthi <chitizawasthi@Chitizs-MacBook-Pro.local>
2025-09-02 02:06:57 +0530

    Initial commit - Spring Boot mini project

---
+++ src/main/java/com/newproject/Journalapp/JournalappApplication.java
@@ -0,0 +1,15 @@
+package com.newproject.Journalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalappApplication {
+
+	public static void main(String[] args)
+	{
+
+		SpringApplication.run(JournalappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/newproject/Journalapp/controller/JournalEntryController.java
@@ -0,0 +1,30 @@
+package com.newproject.Journalapp.controller;
+
+import com.newproject.Journalapp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController
+{
+
+  private Map<Long,JournalEntry>journalEntries=new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll()
+    {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean CreateEntry(@RequestBody JournalEntry myEntry)
+    {
+       journalEntries.put(myEntry.getId(), myEntry);
+       return true;
+    }
+}
