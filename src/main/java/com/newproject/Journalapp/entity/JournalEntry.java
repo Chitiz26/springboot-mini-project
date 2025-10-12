290dd42279069b2e1622e3a5913f2b7435b313fc
chitizawasthi <chitizawasthi@gmail.com>
2025-10-12 23:39:26 +0530

    add properties and some controllers

--- src/main/java/com/newproject/Journalapp/entity/JournalEntry.java
+++ src/main/java/com/newproject/Journalapp/entity/JournalEntry.java
@@ -1,12 +1,15 @@
 package com.newproject.Journalapp.entity;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+@Document
 public class JournalEntry
 {
-    private long id;
+
+    private String id;
     private String title;
     private String content;
 
-    public long getId()
+    public String getId()
     {
         return id;
     }
@@ -18,7 +21,7 @@ public class JournalEntry
     {
         this.title = title;
     }
-    public void setId(long id)
+    public void setId(String id)
     {
         this.id = id;
     }
